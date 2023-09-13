5202b55a8d163b9bb6a7e476dc7c9a6cc56213fb
Elliott Fogg <elliott.fogg@gmail.com>
2023-09-13 14:42:31 +0100

    Uploading all files, useful or not, for access from work laptop

---
+++ main.py
@@ -0,0 +1,9 @@
+# Load input file
+# Create output folder
+# Separate Input Parameters and Requests
+# Split up Requests into separate injection blocks
+# For each block:
+# 		Construct MPS file
+# 		Pass to solver
+#		Output results to folder
+# Construct final executed schedule and output to folder
